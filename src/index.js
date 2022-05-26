09819c8091ec4251c34dc5fd0ddcb6639153b1fa
Anna <bcworldcares@gmail.com>
2022-05-26 22:29:38 +0300

    create react fragment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello App.js</h1>
+    return (
+        <>
+          <h1>Hello App.js</h1>
+          <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+         </ul>
+       </>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

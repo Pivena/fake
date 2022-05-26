a03f4b5a4ea04c098a7b16bab277b3b60cab1677
Anna <bcworldcares@gmail.com>
2022-05-26 22:21:22 +0300

    create component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const h1 = <h1>Hello world</h1>;
+
+function App() {
+    return <h1>Hello App.js</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(h1);
+root.render(<App />);
+

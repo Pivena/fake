f6b975d42f8147cbf2d9f51f62b28aecfe764ffd
Anna <bcworldcares@gmail.com>
2022-05-26 23:18:35 +0300

    export const

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const Header = () => <h1>Hello App.js</h1>
-
-function AppList() {
-    return (
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-    )
-}
-
-function App() {
-    return (
-        <>
-            <Header />
-            <AppList />
-        </>
-    )
-}
+import { App } from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(<App />)

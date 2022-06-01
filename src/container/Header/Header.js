a461730c1a0fba51ed71417eb29d88586d8128ce
Anna <bcworldcares@gmail.com>
2022-06-01 20:41:10 +0300

    add material-ui

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Container } from '@mui/material'
+
+import { Menu } from 'components/Menu/Menu'
+import { CartHeader } from 'components/Cart/CartHeader'
 
 export const Header = () => {
     return (
         <>
-            <h1>Header</h1>
+            <AppBar position="static">
+                <Container>
+                    <Toolbar variant="dense">
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            color="inherit"
+                            component="div"
+                        >
+                            Fake shop
+                        </Typography>
+                        <Menu />
+                        <CartHeader />
+                    </Toolbar>
+                </Container>
+            </AppBar>
         </>
     )
 }

e9e536be620152c192a48ff707809163547ebde0
Anna <bcworldcares@gmail.com>
2022-06-01 21:35:53 +0300

    add module.css

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
-import './CartHeader.css'
+import styles from './CartHeader.module.css'
 
 export const CartHeader = () => {
     return (
-        <div className="cartHeader">
+        <div className={styles.cartHeader}>
             <div>0</div>
             <div>0$</div>
         </div>
